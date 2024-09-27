738a97c5331251d442c8504fc25c10dacb909798
Lichyo <lichyo003@gmail.com>
2024-09-27 21:57:51 +0800

    Homework : get text field input text length.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -14,19 +15,33 @@ public class MainActivity extends AppCompatActivity {
     public void bigger(View view) {
         TextView textView = findViewById(R.id.textView);
         float size = textView.getTextSize();
-        textView.setTextSize(0, ++size);
+        textView.setTextSize(0, size + 5);
+    }
+
+    public void resetText(View view) {
+        TextView textView = findViewById(R.id.textView);
+        textView.setTextSize(0, 40);
+    }
+
+    public void getTextFieldLength(View view) {
+        EditText editText = findViewById(R.id.textField);
+        String string = editText.getText().toString();
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("字串長度: " + string.length());
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);  // JAVA  把 XML 叫出來
+        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        findViewById(R.id.button).setOnClickListener(this::bigger);
+        findViewById(R.id.getTextButton).setOnClickListener(this::getTextFieldLength);
+        findViewById(R.id.textView).setOnClickListener(this::resetText);
+        findViewById(R.id.biggerButton).setOnClickListener(this::bigger);
     }
 }
