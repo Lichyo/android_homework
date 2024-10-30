beffc6df3dd6c4b33b42778b6a93951a8224c021
Lichyo <lichyo003@gmail.com>
2024-10-30 16:44:37 +0800

    Homework : button with counter and terminate process.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,46 +2,74 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    public void bigger(View view) {
-        TextView textView = findViewById(R.id.textView);
-        float size = textView.getTextSize();
-        textView.setTextSize(0, size + 5);
-    }
 
-    public void resetText(View view) {
-        TextView textView = findViewById(R.id.textView);
-        textView.setTextSize(0, 40);
-    }
-
-    public void getTextFieldLength(View view) {
-        EditText editText = findViewById(R.id.textField);
-        String string = editText.getText().toString();
-        TextView textView = findViewById(R.id.textView);
-        textView.setText("字串長度: " + string.length());
-    }
+    private TextView tvCounter;
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        tvCounter = findViewById(R.id.tv_counter);
+        Button btnIncrement = findViewById(R.id.btn_increment);
+        Button btnDecrement = findViewById(R.id.btn_decrement);
+        Button btnExit = findViewById(R.id.btn_exit);
+
+        btnIncrement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                tvCounter.setText(String.valueOf(counter));
+            }
         });
-        findViewById(R.id.getTextButton).setOnClickListener(this::getTextFieldLength);
-        findViewById(R.id.textView).setOnClickListener(this::resetText);
-        findViewById(R.id.biggerButton).setOnClickListener(this::bigger);
+
+        btnIncrement.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                counter += 2;
+                tvCounter.setText(String.valueOf(counter));
+                return true;
+            }
+        });
+
+        btnDecrement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter--;
+                tvCounter.setText(String.valueOf(counter));
+            }
+        });
+
+        btnDecrement.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                counter -= 2;
+                tvCounter.setText(String.valueOf(counter));
+                return true;
+            }
+        });
+
+        tvCounter.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                counter = 0;
+                tvCounter.setText(String.valueOf(counter));
+                return true;
+            }
+        });
+
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
